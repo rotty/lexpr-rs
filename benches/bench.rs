1c5dd1f0065d4764ecfc333e7151ece96f505323
Andreas Rottmann <a.rottmann@gmx.at>
2019-03-01 01:26:08 +0100

    travis: Improve clippy coverage
    Check all features` and targets, using nightly clippy, so we can check
    the benchmarking code, which uses nightly feature `test`.
    
    This exposed the broken benchmarking suite, and a clippy warning in
    the unit tests, both fixed by this commit.

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,16 +4,16 @@ extern crate test;
 
 use test::{black_box, Bencher};
 
-use lexpr::{from_str, from_str_default, parse};
+use lexpr::{from_str, from_str_custom, parse};
 
 #[bench]
 fn bench_float_parsing(b: &mut Bencher) {
-    b.iter(|| black_box(from_str_default("-1.360438755021694e308")));
+    b.iter(|| black_box(from_str("-1.360438755021694e308")));
 }
 
 #[bench]
 fn bench_parsing_keyword_default(b: &mut Bencher) {
-    b.iter(|| black_box(from_str_default("#:some-keyword")))
+    b.iter(|| black_box(from_str("#:some-keyword")))
 }
 
 #[bench]
@@ -21,5 +21,5 @@ fn bench_parsing_keyword_all_styles(b: &mut Bencher) {
     use parse::KeywordStyle::*;
     let options =
         parse::Options::default().with_keyword_styles(&[ColonPrefix, ColonPostfix, Octothorpe]);
-    b.iter(|| black_box(from_str("#:octo :prefix postfix:", &options)));
+    b.iter(|| black_box(from_str_custom("#:octo :prefix postfix:", options.clone())));
 }
