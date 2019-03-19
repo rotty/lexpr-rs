79cb0a4b839408ca9031e69ebd0c04ba2bf3153d
Andreas Rottmann <a.rottmann@gmx.at>
2019-03-19 21:20:15 +0100

    serde-lexpr: Add conversion from and to strings
    These use `lexpr::Value` as an intermediate form, and thus are not as
    efficient as they could be. This also requires a `DeserializeOwned`
    trait bound for `from_str`, unfortunately.

--- serde-lexpr/src/error.rs
+++ serde-lexpr/src/error.rs
@@ -1,9 +1,11 @@
 //! When serializing or deserializing S-expressions goes wrong.
 
 use std::fmt::{self, Debug, Display};
-use std::{error, result};
+use std::{error, io, result};
 
 use serde::{de, ser};
+
+use lexpr::parse;
 
 /// This type represents all possible errors that can occur when
 /// serializing or deserializing S-expression data.
@@ -14,17 +16,23 @@ pub type Result<T> = result::Result<T, Error>;
 
 enum ErrorImpl {
     Message(String),
+    Io(io::Error),
+    Parse(parse::Error),
 }
 
 impl error::Error for Error {
     fn description(&self) -> &str {
-        match *self.0 {
-            ErrorImpl::Message(ref msg) => msg,
+        match &*self.0 {
+            ErrorImpl::Message(msg) => msg,
+            ErrorImpl::Io(e) => e.description(),
+            ErrorImpl::Parse(e) => e.description(),
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
-        match *self.0 {
+        match &*self.0 {
+            ErrorImpl::Io(e) => Some(e),
+            ErrorImpl::Parse(e) => Some(e),
             _ => None,
         }
     }
@@ -32,16 +40,20 @@ impl error::Error for Error {
 
 impl Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self.0 {
+        match &*self.0 {
             ErrorImpl::Message(ref msg) => Display::fmt(msg, f),
+            ErrorImpl::Io(e) => Display::fmt(e, f),
+            ErrorImpl::Parse(e) => Display::fmt(e, f),
         }
     }
 }
 
 impl Debug for Error {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        match *self.0 {
-            ErrorImpl::Message(ref msg) => formatter.debug_tuple("Message").field(msg).finish(),
+        match &*self.0 {
+            ErrorImpl::Message(msg) => formatter.debug_tuple("Message").field(msg).finish(),
+            ErrorImpl::Io(e) => formatter.debug_tuple("Io").field(e).finish(),
+            ErrorImpl::Parse(e) => formatter.debug_tuple("Parse").field(e).finish(),
         }
     }
 }
@@ -63,5 +75,17 @@ impl de::Error for Error {
 impl ser::Error for Error {
     fn custom<T: Display>(msg: T) -> Error {
         Error(Box::new(ErrorImpl::Message(msg.to_string())))
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Error(Box::new(ErrorImpl::Io(e)))
+    }
+}
+
+impl From<parse::Error> for Error {
+    fn from(e: parse::Error) -> Self {
+        Error(Box::new(ErrorImpl::Parse(e)))
     }
 }
---
+++ serde-lexpr/src/ser.rs
@@ -0,0 +1,17 @@
+use serde::Serialize;
+
+use crate::error::Result;
+use crate::value::to_value;
+
+/// Serialize an instance of type `T` into an for S-expression string.
+///
+/// ```
+/// # use serde_lexpr::to_string;
+/// assert_eq!(to_string(&("foo", 1)).unwrap(), r#"("foo" 1)"#.to_string())
+/// ```
+pub fn to_string<T>(value: &T) -> Result<String>
+where
+    T: Serialize,
+{
+    Ok(lexpr::to_string(&to_value(value)?)?)
+}
