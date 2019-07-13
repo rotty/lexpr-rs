4d8b2c892db97a57e3af7d2d31ea25b3224eee21
Andreas Rottmann <a.rottmann@gmx.at>
2019-07-13 23:47:26 +0200

    [S] eval `quote`

--- lexpr/examples/eval.rs
+++ lexpr/examples/eval.rs
@@ -1,7 +1,7 @@
 /// A simple S-expression evaluator.
 use std::{
     collections::HashMap,
-    fmt::{self, Write},
+    fmt::{self, Display, Write},
     io::{self, BufRead},
 };
 
@@ -14,6 +14,9 @@ type OpResult = Result<Gc<Value>, Gc<Value>>;
 pub enum Value {
     Number(Number),
     String(Box<str>),
+    Null,
+    Cons(Box<[Gc<Value>; 2]>),
+    Symbol(Box<str>), // TODO: interning
     PrimOp(Box<Fn(&[Gc<Value>]) -> OpResult>),
 }
 
@@ -40,14 +43,59 @@ impl Value {
     }
 }
 
+impl From<&lexpr::Value> for Value {
+    fn from(v: &lexpr::Value) -> Self {
+        use lexpr::Value::*;
+        match v {
+            Number(n) => Value::Number(n.clone()),
+            String(s) => Value::String(s.clone()),
+            Symbol(s) => Value::Symbol(s.clone()),
+            Cons(cell) => {
+                let (car, cdr) = cell.as_pair();
+                Value::Cons(Box::new([Gc::new(car.into()), Gc::new(cdr.into())]))
+            }
+            Null => Value::Null,
+            _ => unimplemented!(),
+        }
+    }
+}
+
 impl fmt::Display for Value {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Value::Number(n) => write!(f, "{}", n),
+            Value::Symbol(s) => write!(f, "{}", s),
             Value::PrimOp(_) => write!(f, "#<prim-op>"),
+            Value::Null => write!(f, "()"),
+            Value::Cons(cell) => write_cons(f, cell),
             Value::String(s) => lexpr::Value::string(s.as_ref()).fmt(f),
         }
     }
+}
+
+fn write_cons(f: &mut fmt::Formatter, cell: &[Gc<Value>; 2]) -> fmt::Result {
+    use std::ops::Deref;
+
+    f.write_char('(')?;
+    cell[0].fmt(f)?;
+    let mut next = &cell[1];
+    loop {
+        match next.deref() {
+            Value::Null => break,
+            Value::Cons(cell) => {
+                f.write_char(' ')?;
+                cell[0].fmt(f)?;
+                next = &cell[1];
+            }
+            value => {
+                f.write_str(" . ")?;
+                value.fmt(f)?;
+                break;
+            }
+        }
+    }
+    f.write_char(')')?;
+    Ok(())
 }
 
 impl gc::Finalize for Value {
@@ -55,10 +103,43 @@ impl gc::Finalize for Value {
 }
 
 unsafe impl gc::Trace for Value {
-    unsafe fn trace(&self) {}
-    unsafe fn root(&self) {}
-    unsafe fn unroot(&self) {}
-    fn finalize_glue(&self) {}
+    unsafe fn trace(&self) {
+        match self {
+            Value::Cons(cell) => {
+                cell[0].trace();
+                cell[1].trace();
+            }
+            _ => {}
+        }
+    }
+    unsafe fn root(&self) {
+        match self {
+            Value::Cons(cell) => {
+                cell[0].root();
+                cell[1].root();
+            }
+            _ => {}
+        }
+    }
+    unsafe fn unroot(&self) {
+        match self {
+            Value::Cons(cell) => {
+                cell[0].unroot();
+                cell[1].unroot();
+            }
+            _ => {}
+        }
+    }
+    fn finalize_glue(&self) {
+        self.finalize();
+        match self {
+            Value::Cons(cell) => {
+                cell[0].finalize();
+                cell[1].finalize();
+            }
+            _ => {}
+        }
+    }
 }
 
 #[derive(Default)]
@@ -114,6 +195,7 @@ unsafe impl gc::Trace for Env {
 }
 
 macro_rules! make_error {
+    ($fmt:literal) => { Gc::new(Value::String($fmt.into())) };
     ($fmt:literal, $($args:expr),*) => { Gc::new(Value::String(format!($fmt, $($args),*).into())) }
 }
 
@@ -253,17 +335,30 @@ fn proper_list(expr: lexpr::Value) -> Result<Vec<lexpr::Value>, SyntaxError> {
 
 pub fn eval(expr: lexpr::Value, env: Gc<Env>) -> OpResult {
     match expr {
-        lexpr::Value::Symbol(sym) => {
-            env.lookup(&sym).ok_or_else(|| {
-                make_error!("unbound identifier `{}'", sym)
-            })
-        }
+        lexpr::Value::Symbol(sym) => env
+            .lookup(&sym)
+            .ok_or_else(|| make_error!("unbound identifier `{}'", sym)),
         lexpr::Value::Cons(cell) => {
             let (first, rest) = cell.into_pair();
-            let op = eval(first, env.clone())?;
-            let arg_exprs = proper_list(rest).map_err(|e| make_error!("syntax error: {}", e))?;
-            let args = arg_exprs.into_iter().map(|arg| eval(arg, env.clone())).collect::<Result<Vec<_>, _>>()?;
-            apply(op, &args)
+            match first.as_symbol() {
+                Some("quote") => {
+                    let args = proper_list(rest).map_err(|e| make_error!("syntax error: {}", e))?;
+                    if args.len() != 1 {
+                        return Err(make_error!("`quote' expects a single argument"));
+                    }
+                    Ok(Gc::new((&args[0]).into()))
+                }
+                _ => {
+                    let op = eval(first, env.clone())?;
+                    let arg_exprs =
+                        proper_list(rest).map_err(|e| make_error!("syntax error: {}", e))?;
+                    let args = arg_exprs
+                        .into_iter()
+                        .map(|arg| eval(arg, env.clone()))
+                        .collect::<Result<Vec<_>, _>>()?;
+                    apply(op, &args)
+                }
+            }
         }
         lexpr::Value::Number(n) => Ok(Value::Number(n).into()),
         lexpr::Value::String(s) => Ok(Value::String(s).into()),
@@ -274,7 +369,10 @@ pub fn eval(expr: lexpr::Value, env: Gc<Env>) -> OpResult {
 pub fn apply(op: Gc<Value>, args: &[Gc<Value>]) -> OpResult {
     match *op {
         Value::PrimOp(ref op) => op(args),
-        _ => Err(make_error!("non-applicable object in operator position: {}", op)),
+        _ => Err(make_error!(
+            "non-applicable object in operator position: {}",
+            op
+        )),
     }
 }
 
