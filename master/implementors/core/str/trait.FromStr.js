943d7b657f5f6db66766106b4889e694145995ac
Travis CI User <travis@example.org>
2019-07-13 22:52:38 +0000

    ghp-upload script

--- master/implementors/core/str/trait.FromStr.js
+++ master/implementors/core/str/trait.FromStr.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["lexpr"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"enum\" href=\"lexpr/value/enum.Value.html\" title=\"enum lexpr::value::Value\">Value</a>",synthetic:false,types:["lexpr::value::Value"]},];
 implementors["proc_macro2"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/trait.FromStr.html\" title=\"trait core::str::FromStr\">FromStr</a> for <a class=\"struct\" href=\"proc_macro2/struct.TokenStream.html\" title=\"struct proc_macro2::TokenStream\">TokenStream</a>",synthetic:false,types:["proc_macro2::TokenStream"]},];
 
             if (window.register_implementors) {
