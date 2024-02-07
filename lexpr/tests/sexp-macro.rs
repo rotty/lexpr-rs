3355a09b50b3510b2a62b1429eeeafed768b4d7b
samuel <53528911+samuel-jimenez@users.noreply.github.com>
2024-02-07 10:17:57 -0600

    Accept colon-prefixed keywords in `sexp` macro
    Implementation by @samuel-jimenez in PR #96.
    
    Duplicate of #96, closes #99.

--- lexpr/tests/sexp-macro.rs
+++ lexpr/tests/sexp-macro.rs
@@ -19,6 +19,8 @@ fn test_symbols() {
 fn test_keywords() {
     assert_eq!(sexp!(#:foo), Value::keyword("foo"));
     assert_eq!(sexp!(#:"a-keyword"), Value::keyword("a-keyword"));
+    assert_eq!(sexp!(:foo), Value::keyword("foo"));
+    assert_eq!(sexp!(:"a-keyword"), Value::keyword("a-keyword"));
 }
 
 #[test]
@@ -35,6 +37,14 @@ fn test_cons() {
     assert_eq!(
         sexp! {((a . 256.0))},
         Value::list(vec![Value::cons(Value::symbol("a"), Value::from(256.0))])
+    );
+    assert_eq!(
+        sexp!((#:foo)),
+        Value::cons(Value::keyword("foo"), Value::Null)
+    );
+    assert_eq!(
+        sexp!((:foo)),
+        Value::cons(Value::keyword("foo"), Value::Null)
     );
 }
 
@@ -68,6 +78,7 @@ fn test_special_tokens() {
     assert_eq!(sexp!(...), Value::symbol("..."));
     assert_eq!(sexp!(.++), Value::symbol(".++"));
     assert_eq!(sexp!(!$%&*+-./:<=>?@^~), Value::symbol("!$%&*+-./:<=>?@^~"));
+    assert_eq!(sexp!(:!$%&*+-./<=>?@^~), Value::symbol(":!$%&*+-./<=>?@^~"));
     assert_eq!(
         sexp!((+ 1 2)),
         Value::list(vec![Value::symbol("+"), 1.into(), 2.into()])
